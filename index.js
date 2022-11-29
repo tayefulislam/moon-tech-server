efed66b9104e399f79df145daa415a3cf580b352
tayefulislam <kazitayefulislam12@gmail.com>
2022-11-29 10:39:09 +0600

    innitial server

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const uri = process.env.URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+  try {
+    await client.connect();
+
+    const productsCollection = client.db("MoodTech1").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const products = await productsCollection.find({}).toArray();
+
+      res.status(200).json(products);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log("server is running at", port);
+});
